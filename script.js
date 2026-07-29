23b13a18b58ed066b1820cdd20495ebc0cc7e6c6
Mohd Azam | Full Stack Developer <azamraza7037@gmail.com>
2026-07-30 01:04:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,49 @@
 console.log("AZAM AI v6 Loaded");
+/*=========================
+COUNTER ANIMATION
+=========================*/
+
+const counters = document.querySelectorAll(".counter");
+
+const counterObserver = new IntersectionObserver((entries)=>{
+
+    entries.forEach(entry=>{
+
+        if(!entry.isIntersecting) return;
+
+        const counter = entry.target;
+        const target = +counter.dataset.target;
+
+        let current = 0;
+
+        const increment = target / 100;
+
+        const updateCounter = ()=>{
+
+            current += increment;
+
+            if(current < target){
+
+                counter.innerText = Math.ceil(current);
+
+                requestAnimationFrame(updateCounter);
+
+            }else{
+
+                counter.innerText = target + "+";
+
+            }
+
+        };
+
+        updateCounter();
+
+        counterObserver.unobserve(counter);
+
+    });
+
+},{
+    threshold:.5
+});
+
+counters.forEach(counter=>counterObserver.observe(counter));
